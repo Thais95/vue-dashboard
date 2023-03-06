d46d4f19bf0705ec717147cc3a808bd766e493e6
Thais95 <thaiscdaraujo95@gmail.com>
2023-03-06 03:09:07 -0300

    chore: adding header and making components responsive

--- src/components/MenuBar.vue
+++ src/components/MenuBar.vue
@@ -1,18 +1,20 @@
 <template>
 <Menubar class="menu">
   <template #start>
-    <div class="logo-name">
-      <img src="../assets/logo.ico" alt="Logo">
-      <span>Nome da Empresa</span>
+    <div class="start-wrapper">
+      <div class="logo-name">
+        <img src="../assets/logo.ico" alt="Logo">
+        <span>Nome da Empresa</span>
+      </div>
+      <nav class="nav">
+        <ul class="nav-links">
+          <li>Início</li>
+          <li>Disputas</li>
+          <li>Minhas Licitações</li>
+          <li>Extra</li>
+        </ul>
+      </nav>
     </div>
-    <nav class="nav">
-      <ul>
-        <li>Início</li>
-        <li>Disputas</li>
-        <li>Minhas Licitações</li>
-        <li>Extra</li>
-      </ul>
-    </nav>
   </template>
   <template #end>
     <div class="signout">
@@ -31,18 +33,29 @@ export default {
 
 <style lang="scss" scoped>
 .menu {
-  widows: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  gap: 20px;
+  height: 80px;
+  @media screen and (max-width: 760px) {
+    height: auto;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+    align-items: center;
+  }
+  .start-wrapper {
+    display: flex;
+    align-items: center;
+    gap: 62px;
+    @media screen and (max-width: 760px) {
+      flex-direction: column;
+      gap: 20px;
+    }
+  }
   .logo-name {
     display: flex;
     flex-direction: row;
     align-items: center;
     justify-content: space-between !important;
-    height: 60px;
-    gap: 20px;
+    gap: 10px;
     cursor: default;
     user-select: none;
     img {
@@ -55,15 +68,46 @@ export default {
       color: var(---primary);
     }
   }
+  .nav {
+    .nav-links {
+      display: flex;
+      flex-direction: row;
+      gap: 18px;
+      @media screen and (max-width: 760px) {
+        flex-direction: column;
+        align-items: center;
+        gap: 20px;
+      }
+      li:first-child {
+        color: var(---primary);
+      }
+      li {
+        font-weight: 500;
+        cursor: pointer;
+        color: darken($color: #008cff, $amount: 26);
+        transition: 0.2s;
+        &:hover {
+          transition: 0.2s;
+          color: var(---accent);
+        }
+      }
+    }
+  }
   .signout {
     display: flex;
     align-items: center;
     gap: 10px;
     font-weight: 600;
     cursor: pointer;
+    color: darken($color: #008cff, $amount: 26);
+    transition: 0.2s;
+    @media screen and (max-width: 760px) {
+      margin: 0 auto;
+      width: 100%;
+    }
     &:hover {
-      color: var(---primary);
       transition: 0.2s;
+      color: var(---accent);
     }
   }
 }
